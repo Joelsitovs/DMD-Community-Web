259d700220dc67cde9d21be030b8d22cd9c53de2
Joel <alanjoelt43@gmail.com>
2024-10-30 15:29:56 +0100

    dada

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ while ($row = $resultado->fetch_assoc()) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Torneos</title>
-    
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=arrow_forward" />
     <link rel="stylesheet" href="styles.css">
 </head>
 
