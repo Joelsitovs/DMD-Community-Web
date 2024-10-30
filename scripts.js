d43843a1f4b0a7bfba1042c59ee7b295c90d090b
Joel <alanjoelt43@gmail.com>
2024-10-30 14:46:03 +0100

    as

--- scripts.js
+++ scripts.js
@@ -1,18 +1,21 @@
-new Swiper('.card-wrapper', {
-    // Optional parameters
-
-    loop: true,
-  
-    // If we need pagination
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 20,
     pagination: {
       el: '.swiper-pagination',
+      clickable: true,
     },
-  
-    // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-  
-   
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
   });
+  
