b342e850fb2bde652b626b1b2e58aa571bcd0fce
Joel <alanjoelt43@gmail.com>
2024-10-30 15:39:05 +0100

    ad

--- scripts.js
+++ scripts.js
@@ -11,6 +11,16 @@ new Swiper('.card-wrapper', {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-  
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    }
     
   });
