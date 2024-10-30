27b4701861b7043dd4be16daf17640f5c5aee8c7
Joel <alanjoelt43@gmail.com>
2024-10-30 15:35:06 +0100

    asd

--- index.php
+++ index.php
@@ -17,15 +17,16 @@ while ($row = $resultado->fetch_assoc()) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Torneos</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=arrow_forward" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
     <link rel="stylesheet" href="styles.css">
 </head>
 
 <body>
-    <div class="container ">
+    <div class="container swiper">
         <div class="card-wrapper">
-            <ul class="card-list">
+            <ul class="card-list swiper-wrapper">
                 <?php foreach ($torneos as $row) { ?>
-                <li class="card-item">
+                <li class="card-item swiper-slide">
                     <a href="#" class="card-link">
                         <img src="<?php echo $row['imagen']; ?>" alt="Card Image" class="card-image">
                         <h2 class="card-title"><?php echo $row['nombre']; ?></h2>
@@ -37,9 +38,14 @@ while ($row = $resultado->fetch_assoc()) {
                 </li>
                 <?php } ?>
             </ul>
-      
+            <div class="swiper-pagination"></div>
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
         </div>
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
 
 </body>
 
