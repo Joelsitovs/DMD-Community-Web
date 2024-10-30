edc0f0d22505060eb57b6d29bc22370454a8ef41
Joel <alanjoelt43@gmail.com>
2024-10-30 09:30:38 +0100

    BD

--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
-$dbname = 'u328800997_BdDmon';
-$user = 'u328800997_root3';
-$pass = 'asyf7F9ckp7&hET';
+$dbname = 'u328800997_Dmondejarm';
+$user = 'u328800997_rootDmondejarm';
+$pass = 'F5^Z#rU1u&h';
 
 $conexion = new mysqli($host, $user, $pass, $dbname);
 if ($conexion->connect_error) {
