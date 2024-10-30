7dff7147229d71f31197f557703ca8fc2c2c4e0f
Joel <alanjoelt43@gmail.com>
2024-10-30 11:15:04 +0100

    css

--- scripts.js
+++ scripts.js
@@ -1,15 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-  new Glide("#glide", {
-    type: "carousel", // Tipo de carrusel
-    startAt: 0, // Comienza en el primer elemento
-    perView: 3, // Número de elementos visibles
-    breakpoints: {
-      800: {
-        perView: 1, // Muestra 1 elemento en pantallas pequeñas
-      },
-      1200: {
-        perView: 2, // Muestra 2 elementos en pantallas medianas
-      },
-    },
-  }).mount();
+window.addEventListener('load', function() {
+  new Glider(document.querySelector('.carousel__lista'));
 });
