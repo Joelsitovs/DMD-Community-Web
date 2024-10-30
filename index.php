225d9f47220ba693280f651f712ee4f20f88445a
Joel <alanjoelt43@gmail.com>
2024-10-30 13:35:19 +0100

    a

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php
 include 'conexion.php';
 
-$consulta = "SELECT nombre, fecha_inicio, Premio, Precio_inscripcion, imagen FROM torneos LIMIT 1";
+$consulta = "SELECT nombre, fecha_inicio, Premio, Precio_inscripcion, imagen FROM torneos Limit 1";
 $resultado = $conexion->query($consulta);
 
-
+$torneos = [];
+while ($row = $resultado->fetch_assoc()) {
+    $torneos[] = $row;
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,7 +15,7 @@ $resultado = $conexion->query($consulta);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Torneos</title>
+    <title>Document</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=arrow_forward" />
     <link rel="stylesheet" href="./styles.css">
 </head>
@@ -21,22 +24,20 @@ $resultado = $conexion->query($consulta);
     <div class="container">
         <div class="card-wrapper">
             <ul class="card-list">
-                     <?php while ($row = $resultado->fetch_assoc()){ ?>
-                        <li class="card-item">
-                            <a href="#" class="card-link">
-                                <img src="<?php echo $row ['imagen'] ?>" alt="" class="card-image">
-                                <h2 class="card-title"><?php echo $row ['nombre']?></h2>
-                                <p class="card-text">Fecha de inicio: <?php echo $row['fecha_inicio']?></p>
-                                <p class="card-text">Premio: <?php echo $row['Premio']?></p>
-                                <p class="card-text">Precio de inscripción: $<?php echo $row ['Precio_inscripcion'] ?></p>
-                                <button class="card-button material-symbols-outlined">arrow_forward</button>
-                            </a>
-                        </li> <?php } ?>
-     
-     
-                    <li class="card-item">No se encontraron torneos.</li>
-                   
-            </ul>   
+                <?php foreach ($torneos as $row) { ?>
+                <li class="card-item">
+                    <a href="#" class="card-link">
+                        <img src="<?php echo ($row['imagen']);?> " alt="" class="card-image">
+                        <h2 class="card-title"><?php echo ($row['nombre']);  ?></h2>
+                        <p class="card-text">Fecha de inicio: 12/12/2020</p>
+                        <p class="card-text">Premio</p>
+                        <p class="card-text">Precio de inscripción: $100</p>
+                        <button class="card-button
+                        material-symbols-outlined"> arrow_forward</button>
+                    </a>
+                </li>
+                <?php } ?>
+            </ul>
         </div>
     </div>
 
