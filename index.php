c4eada24a28b3c5e9ad6dabcaef03eb6eb1d6dcf
Joel <alanjoelt43@gmail.com>
2024-10-30 12:42:59 +0100

    css

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ while ($row = $resultado->fetch_assoc()) {
     <title>Document</title>
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=arrow_forward" />
-    <link rel="stylesheet" href="scripts.js">
+    <link rel="stylesheet" href="styles.css">
 </head>
 
 <body>
