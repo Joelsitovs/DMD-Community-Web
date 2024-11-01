9b39ccf6da1138e124058919c36c9c22ee8ad2f1
Joel <alanjoelt43@gmail.com>
2024-11-01 14:19:53 +0100

    prueba google

--- user/config.php
+++ user/config.php
@@ -2,6 +2,6 @@
 
 $clientID ="1094322288695-hn9ddm8gjbsl54gpjp17dm7l233h786j.apps.googleusercontent.com";
 $clientSecret = "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";
-$redirectURI = 	"https://pruebas.caowthing.es/user/login.php";
+$redirectURI = 	"https://pruebas.caowthing.es/user/logingoogle.php";
 ?>
 
