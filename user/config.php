098be5404a51c0de8098fef150387a06efbf404f
Joel <alanjoelt43@gmail.com>
2024-11-01 13:28:20 +0100

    Registro y login con google

--- user/config.php
+++ user/config.php
@@ -2,5 +2,5 @@
 
 $clientID ="1094322288695-hn9ddm8gjbsl54gpjp17dm7l233h786j.apps.googleusercontent.com";
 $clientSecret = "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";
-$redirectURI = "https://pruebas.caowthing.es/user/usersesion.php";
+$redirectURI = "https://pruebas.caowthing.es/ola.php";
 
