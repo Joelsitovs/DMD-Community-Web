8701a84839e733605d349997712f8f355fd991c7
Yoel <79540811+Joelsitovs@users.noreply.github.com>
2024-11-04 00:25:17 +0100

    Actualizar index.php

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ while ($row = $resultado->fetch_assoc()) {
     $torneos[] = $row;
 }
 
+
 $consulta2 = "SELECT nombre,enlace,icono FROM redessociales";
 $resultado2 = $conexion->query($consulta2);
 
