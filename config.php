e9ea9354d30d2362b9737e3efd0fe62cddef11de
Joel <alanjoelt43@gmail.com>
2024-11-04 18:18:31 +0100

    add

--- config.php
+++ config.php
@@ -1,15 +1,23 @@
 <?php
-
 function loadEnv($filePath = __DIR__ . '/.env') {
     if (!file_exists($filePath)) {
-        throw new Exception("Archivo .env no encontrado.");
+        throw new Exception("Archivo .env no encontrado en la ruta: $filePath");
     }
 
     $lines = file($filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
     foreach ($lines as $line) {
-        if (strpos(trim($line), '#') === 0) continue; // Ignora comentarios
+        // Ignora líneas que son comentarios
+        if (strpos(trim($line), '#') === 0) continue;
+
+        // Asegúrate de que haya un signo de igual en la línea
+        if (strpos($line, '=') === false) {
+            continue; // O puedes lanzar una excepción
+        }
+
+        // Divide la línea en clave y valor
         list($key, $value) = explode('=', $line, 2);
-        $_ENV[trim($key)] = trim($value); // Asigna cada variable a $_ENV
+        // Asigna cada variable a $_ENV
+        $_ENV[trim($key)] = trim($value);
     }
 }
 
@@ -17,6 +25,8 @@ function loadEnv($filePath = __DIR__ . '/.env') {
 loadEnv();
 
 // Ahora puedes usar las variables de entorno en tu código
-$clientID = $_ENV['CLIENT_ID'];
-$clientSecret = $_ENV['CLIENT_SECRET'];
-$redirectURI = $_ENV['REDIRECT_URI'];
+$clientID = $_ENV['CLIENT_ID'] ?? null; // Usa null coalescing para manejar la ausencia de la clave
+$clientSecret = $_ENV['CLIENT_SECRET'] ?? null;
+$redirectURI = $_ENV['REDIRECT_URI'] ?? null;
+
+
